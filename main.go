0ce78fd6abe60fc8477e9000ae6b882374adfa77
flowerinthenight <root@flowerinthenight.com>
2025-02-18 18:31:43 +0900

    add gcp support

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"strings"
 	"sync"
 	"syscall"
 
@@ -23,11 +24,14 @@ var (
 	green = color.New(color.FgGreen).SprintFunc()
 	red   = color.New(color.FgRed).SprintFunc()
 
-	stdout bool
-	stderr bool
-	idFile string
-	mtx    sync.Mutex
-	cs     map[string]*exec.Cmd
+	vendor     string
+	gcpProject string // for GCP only
+	gcpRegion  string // for GCP only
+	stdout     bool
+	stderr     bool
+	idFile     string
+	mtx        sync.Mutex
+	cs         map[string]*exec.Cmd
 
 	rootCmd = &cobra.Command{
 		Use:   "g-ssh-cmd <group-name> 'cmd'",
@@ -40,9 +44,14 @@ var (
 	}
 )
 
+// AWS-related definitions
 type instanceT struct {
+	// AWS
 	InstanceId      string
 	PublicIpAddress string
+
+	// GCP
+	Instance string `json:"instance"`
 }
 
 type asgT struct {
@@ -59,6 +68,12 @@ type reservationT struct {
 
 type instW struct {
 	Reservations []reservationT
+}
+
+// GCP-related definitions
+type migW struct {
+	Name   string `json:"name"`
+	Region string `json:"region"`
 }
 
 func main() {
@@ -79,9 +94,12 @@ func main() {
 		os.Exit(0)
 	}()
 
+	rootCmd.Flags().StringVar(&vendor, "vendor", "aws", "target vendor, values: 'aws', 'gcp'")
 	rootCmd.Flags().StringVar(&idFile, "id-file", "", "identity file, input to -i in ssh")
 	rootCmd.Flags().BoolVar(&stdout, "stdout", true, "print stdout output")
 	rootCmd.Flags().BoolVar(&stderr, "stderr", true, "print stderr output")
+	rootCmd.Flags().StringVar(&gcpProject, "gcp-project", "", "GCP project name, optional (inferred)")
+	rootCmd.Flags().StringVar(&gcpRegion, "gcp-region", "", "GCP region, optional (inferred)")
 	rootCmd.Execute()
 }
 
@@ -96,77 +114,148 @@ func run(cmd *cobra.Command, args []string) {
 		return
 	}
 
-	xcmd := exec.Command(
-		"aws",
-		"autoscaling",
-		"describe-auto-scaling-groups",
-		"--auto-scaling-group-name",
-		args[0],
-	)
-
-	out, err := xcmd.CombinedOutput()
-	if err != nil {
-		fail(err)
-		return
-	}
-
-	var t asgW
-	err = json.Unmarshal(out, &t)
-	if err != nil {
-		fail(err)
-		return
-	}
-
 	cs = make(map[string]*exec.Cmd)
-	for _, i := range t.AutoScalingGroups {
-		var wg sync.WaitGroup
-		for _, j := range i.Instances {
-			wg.Add(1)
-			go func(id string) {
-				defer wg.Done()
-				xcmd = exec.Command(
-					"aws",
-					"ec2",
-					"describe-instances",
-					"--instance-ids",
-					id,
-				)
 
-				iout, err := xcmd.CombinedOutput()
-				if err != nil {
-					fail(err)
-					return
-				}
+	switch vendor {
+	case "aws":
+		xcmd := exec.Command(
+			"aws",
+			"autoscaling",
+			"describe-auto-scaling-groups",
+			"--auto-scaling-group-name",
+			args[0],
+		)
 
-				var v instW
-				err = json.Unmarshal(iout, &v)
-				if err != nil {
-					fail(err)
-					return
-				}
-
-				for _, x := range v.Reservations {
-					for _, y := range x.Instances {
-						addcmd := exec.Command(
-							"ssh",
-							"-i",
-							idFile,
-							"-o",
-							"StrictHostKeyChecking=accept-new",
-							fmt.Sprintf("ec2-user@%v", y.PublicIpAddress),
-							"-t",
-							args[1],
-						)
-
-						mtx.Lock()
-						cs[id] = addcmd
-						mtx.Unlock()
-					}
-				}
-			}(j.InstanceId)
+		out, err := xcmd.CombinedOutput()
+		if err != nil {
+			fail(err)
+			return
 		}
 
-		wg.Wait()
+		var t asgW
+		err = json.Unmarshal(out, &t)
+		if err != nil {
+			fail(err)
+			return
+		}
+
+		for _, i := range t.AutoScalingGroups {
+			var wg sync.WaitGroup
+			for _, j := range i.Instances {
+				wg.Add(1)
+				go func(id string) {
+					defer wg.Done()
+					xcmd = exec.Command(
+						"aws",
+						"ec2",
+						"describe-instances",
+						"--instance-ids",
+						id,
+					)
+
+					iout, err := xcmd.CombinedOutput()
+					if err != nil {
+						fail(err)
+						return
+					}
+
+					var v instW
+					err = json.Unmarshal(iout, &v)
+					if err != nil {
+						fail(err)
+						return
+					}
+
+					for _, x := range v.Reservations {
+						for _, y := range x.Instances {
+							addcmd := exec.Command(
+								"ssh",
+								"-i",
+								idFile,
+								"-o",
+								"StrictHostKeyChecking=accept-new",
+								fmt.Sprintf("ec2-user@%v", y.PublicIpAddress),
+								"-t",
+								args[1],
+							)
+
+							mtx.Lock()
+							cs[id] = addcmd
+							mtx.Unlock()
+						}
+					}
+				}(j.InstanceId)
+			}
+
+			wg.Wait()
+		}
+	case "gcp":
+		var line strings.Builder
+		if gcpProject == "" || gcpRegion == "" {
+			fmt.Fprintf(&line, "gcloud compute instance-groups managed list ")
+			fmt.Fprintf(&line, "--filter=\"name=('%v')\" --format json", args[0])
+			out, err := exec.Command("bash", "-c", line.String()).CombinedOutput()
+			if err != nil {
+				fail(err)
+				return
+			}
+
+			var t []migW
+			err = json.Unmarshal(out, &t)
+			if err != nil {
+				fail(err)
+				return
+			}
+
+			for _, v := range t {
+				ss := strings.Split(v.Region, "/")
+				// Fmt: https://www.googleapis.com/compute/v1/projects/v/regions/v
+				if len(ss) >= 9 {
+					gcpProject = ss[6]
+					gcpRegion = ss[8]
+				}
+			}
+		}
+
+		line.Reset()
+		fmt.Fprintf(&line, "gcloud compute instance-groups managed list-instances rmig ")
+		fmt.Fprintf(&line, "--project=%v --region=%v --format json", gcpProject, gcpRegion)
+		out, err := exec.Command("bash", "-c", line.String()).CombinedOutput()
+		if err != nil {
+			fail(err)
+			return
+		}
+
+		var v []instanceT
+		err = json.Unmarshal(out, &v)
+		if err != nil {
+			fail(err)
+			return
+		}
+
+		for _, i := range v {
+			// Fmt: https://www.googleapis.com/compute/v1/projects/v/zones/v/instances/name
+			ss := strings.Split(i.Instance, "/")
+			if len(ss) < 11 {
+				continue
+			}
+
+			name := ss[10]
+			zone := ss[8]
+			info(name, zone)
+
+			var add strings.Builder
+			fmt.Fprintf(&add, "gcloud compute ssh --zone %v %v ", zone, name)
+			fmt.Fprintf(&add, "--project %v --quiet --command='%v' -- -t", gcpProject, args[1])
+			addcmd := exec.Command("bash", "-c", add.String())
+
+			mtx.Lock()
+			cs[name] = addcmd
+			mtx.Unlock()
+		}
+	default:
+		fail("invalid vendor")
+		return
 	}
 
 	if len(cs) == 0 {
